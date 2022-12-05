dfa40f6122347fc8d0fa2fe6b002a81b50ac3f62
codersubhji <subhji75088@gmail.com>
2022-12-05 19:21:54 +0530

    submit

---
+++ Assignment-30/index5.py
@@ -0,0 +1,11 @@
+# <!-- 5. Embed Python swap program in html document
+# Note: You can either use C or Python Program . -->
+
+
+a,b=int(input("Enter first number :")),int(input("Enter second number :"))
+a=a+b
+b=a-b
+a=a-b
+
+print(a,b, end=" ")
+
